a8d435a9ae4476d4bc4e0da76dd7a17beba19e2d
Jorge Montesinos <osmarmontes50@gmail.com>
2023-06-09 23:48:20 -0500

    fix at orders route

--- routes/orders.js
+++ routes/orders.js
@@ -2,6 +2,9 @@ const {
   requireAuth,
 } = require('../middleware/auth');
 
+const { createOrder, getOrder, getOrders, updateOrder, deleteOrder } = require('../controller/orders');
+const { get } = require('mongoose');
+
 /** @module orders */
 module.exports = (app, nextMain) => {
   /**
@@ -30,8 +33,7 @@ module.exports = (app, nextMain) => {
    * @code {200} si la autenticación es correcta
    * @code {401} si no hay cabecera de autenticación
    */
-  app.get('/orders', requireAuth, (req, resp, next) => {
-  });
+  app.get('/orders', requireAuth, getOrders);
 
   /**
    * @name GET /orders/:orderId
@@ -54,8 +56,7 @@ module.exports = (app, nextMain) => {
    * @code {401} si no hay cabecera de autenticación
    * @code {404} si la orden con `orderId` indicado no existe
    */
-  app.get('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
+  app.get('/orders/:orderId', requireAuth, getOrder);
 
   /**
    * @name POST /orders
@@ -83,8 +84,7 @@ module.exports = (app, nextMain) => {
    * @code {400} no se indica `userId` o se intenta crear una orden sin productos
    * @code {401} si no hay cabecera de autenticación
    */
-  app.post('/orders', requireAuth, (req, resp, next) => {
-  });
+  app.post('/orders', requireAuth, createOrder);
 
   /**
    * @name PUT /orders
@@ -114,8 +114,7 @@ module.exports = (app, nextMain) => {
    * @code {401} si no hay cabecera de autenticación
    * @code {404} si la orderId con `orderId` indicado no existe
    */
-  app.put('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
+  app.put('/orders/:orderId', requireAuth, updateOrder);
 
   /**
    * @name DELETE /orders
@@ -138,8 +137,7 @@ module.exports = (app, nextMain) => {
    * @code {401} si no hay cabecera de autenticación
    * @code {404} si el producto con `orderId` indicado no existe
    */
-  app.delete('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
+  app.delete('/orders/:orderId', requireAuth, deleteOrder);
 
   nextMain();
 };
